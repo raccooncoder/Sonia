2979652df8c3febd7f2389827056d686dd99fac8
raccooncoder <shaban.mahmud@yandex.ru>
2019-03-23 02:09:58 +0300

    Readability enhancements

--- Project/utils.py
+++ Project/utils.py
@@ -1,14 +1,15 @@
 import numpy as np
-import cv2
-import os
 
 def transpose_range(samples):
 	merged_sample = np.zeros_like(samples[0])
+
 	for sample in samples:
 		merged_sample = np.maximum(merged_sample, sample)
+
 	merged_sample = np.amax(merged_sample, axis=0)
 	min_note = np.argmax(merged_sample)
 	max_note = merged_sample.shape[0] - np.argmax(merged_sample[::-1])
+
 	return min_note, max_note
 
 def generate_add_centered_transpose(samples):
@@ -17,8 +18,10 @@ def generate_add_centered_transpose(samples):
 	s = num_notes // 2 - (max_note + min_note) // 2
 	out_samples = samples
 	out_lens = [len(samples), len(samples)]
+
 	for i in range(len(samples)):
 		out_sample = np.zeros_like(samples[i])
-		out_sample[:,min_note+s:max_note+s] = samples[i][:,min_note:max_note]
+		out_sample[:, min_note + s : max_note + s] = samples[i][:, min_note : max_note]
 		out_samples.append(out_sample)
+
 	return out_samples, out_lens
